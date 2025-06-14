1862a64bf61fd09fe184cbd8da34be4b73cb024b
Suraj S Neelagar <71820711+surajneelagar@users.noreply.github.com>
2025-06-14 15:38:28 +0530

    masterData

---
+++ models/masterData.go
@@ -0,0 +1,31 @@
+package models
+
+import "gorm.io/gorm"
+
+// MasterData represents the structure of the master data.
+type MasterData struct { // Correct: Uppercase M for export
+	Countries        []DataItem `json:"countries"`
+	DeviceTypes      []DataItem `json:"deviceTypes"`
+	MachineModels    []DataItem `json:"machineModels"`
+	Status           []DataItem `json:"status"`
+	ReturnableStatus []DataItem `json:"returnableStatus"`
+	ReturnReason     []DataItem `json:"returnReason"`
+}
+
+// DataItem represents a generic item with a name and a code.
+type DataItem struct { // Correct: Uppercase D for export
+	Name string `json:"name"`
+	Code string `json:"code"`
+}
+
+type Country struct { // Correct: Uppercase C for export
+	gorm.Model
+	Name string `json:"name"`
+	Code string `json:"code" gorm:"unique"`
+}
+
+type DeviceType struct { // Correct: Uppercase D for export
+	gorm.Model
+	Name string `json:"name"`
+	Code string `json:"code" gorm:"unique"`
+}

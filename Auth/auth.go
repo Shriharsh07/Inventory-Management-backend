dd76cc6a9a792f1b30f35edfb117cd5d01497cd7
Shriharsh07 <shriharshdpattar@gmail.com>
2025-06-14 11:57:13 +0530

    Added auth controller and jwt middleware

---
+++ Auth/auth.go
@@ -0,0 +1,100 @@
+package Auth
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+var jwtKey = []byte("pXIl2AQ7AOyXgqwPF69uDXdjEBeMVv826QNQ0OjNNIs=")
+
+// User model for GORM
+type User struct {
+	ID       uuid.UUID `gorm:"primaryKey" json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email" gorm:"unique"`
+	Password string    `json:"password"`
+}
+
+type Credentials struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// DB instance (should be set from main.go)
+var DB *gorm.DB
+
+func Signup(w http.ResponseWriter, r *http.Request) {
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	// Check if user already exists
+	var existing User
+	if err := DB.Where("email = ?", user.Email).First(&existing).Error; err == nil {
+		http.Error(w, "User already exists", http.StatusBadRequest)
+		return
+	}
+
+	// Hash the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Error hashing password", http.StatusInternalServerError)
+		return
+	}
+
+	user = User{
+		ID:       uuid.New(),
+		Name:     user.Name,
+		Email:    user.Email,
+		Password: string(hashedPassword),
+	}
+
+	if err := DB.Create(&user).Error; err != nil {
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Signup successful"})
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+	if err := json.NewDecoder(r.Body).Decode(&creds); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	var user User
+	if err := DB.Where("email = ?", creds.Email).First(&user).Error; err != nil {
+		http.Error(w, "Incorrect email or password", http.StatusUnauthorized)
+		return
+	}
+
+	// Compare hashed password
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(creds.Password)); err != nil {
+		http.Error(w, "Incorrect email or password", http.StatusUnauthorized)
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": creds.Email,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		http.Error(w, "Could not create token", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module github.com/Shriharsh07/InventoryManagement
 go 1.23.9
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.2
+	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
 	github.com/swaggo/http-swagger v1.3.4
+	golang.org/x/crypto v0.39.0
 	gorm.io/driver/mysql v1.6.0
 	gorm.io/gorm v1.30.0
 )
+
+require github.com/stretchr/testify v1.8.1 // indirect
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
@@ -24,8 +29,8 @@ require (
 	github.com/rs/cors v1.11.1
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
 	github.com/swaggo/swag v1.8.1 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/text v0.20.0 // indirect
-	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	golang.org/x/tools v0.33.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
